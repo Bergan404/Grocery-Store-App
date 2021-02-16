8950b116011fe880f3cf30cb8559bac9c782ff59
Bergan Oudshoorn <Bergan00@gmail.com>
2021-02-16 18:43:47 -0500

    phase 3 all done

--- grocery-store-master/src/components/ProduceList/ProduceDetails.js
+++ grocery-store-master/src/components/ProduceList/ProduceDetails.js
@@ -1,5 +1,9 @@
+import {addToCart} from '../../store/cart'
+import {useDispatch} from 'react-redux'
+
 function ProduceDetails({ produce }) {
   const cartItem = {};
+  const dispatch = useDispatch();
 
   return (
     <li className="produce-details">
@@ -11,6 +15,7 @@ function ProduceDetails({ produce }) {
           <i className={"fas fa-heart"} />
         </button>
         <button
+        onClick={(() => dispatch(addToCart(produce.id)))}
           className={"plus-button" + (cartItem ? " selected" : "")}
         >
           <i className="fas fa-plus" />
--- grocery-store-master/src/store/cart.js
+++ grocery-store-master/src/store/cart.js
@@ -13,8 +13,7 @@ export default function
 cartReducer(state = {}, action) {
     switch(action.type) {
         case ADD_CART:
-            const produceIds = action.produceId;
-            return {...state, produceIds: {id: action.produceId , count: 1}}
+            return {...state, [action.produceId]: {id: action.produceId , count: 1}}
         default:
             return state;
     }
