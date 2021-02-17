cd7a392fb8a1d21f828408cbde9374e36c1aae0f
cygnus33065 <cygnus_33065@yahoo.com>
2021-02-16 19:23:52 -0500

    phase 4

--- grocery-store-master/src/store/cart.js
+++ grocery-store-master/src/store/cart.js
@@ -1,9 +1,17 @@
 
 const ADD_CART = 'cart/ADD'
+const REMOVE_CART = 'cart/REMOVE'
 
 export const addToCart = (id) => {
     return {
         type: ADD_CART,
+        produceId: id
+    }
+}
+
+export const removeFromCart = (id) => {
+    return {
+        type: REMOVE_CART,
         produceId: id
     }
 }
@@ -14,6 +22,11 @@ cartReducer(state = {}, action) {
     switch(action.type) {
         case ADD_CART:
             return {...state, [action.produceId]: {id: action.produceId , count: 1}}
+        case REMOVE_CART:
+            const newCart = {...state}
+            delete newCart.[action.produceId]
+            console.log(newCart)
+            return newCart
         default:
             return state;
     }
